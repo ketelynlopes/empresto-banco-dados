1abf4ddcbd00d365cb057ac9cf350dd698f4a60a
ketelynlopes <ketelynlopespereira@gmail.com>
2026-09-21 09:00:49 -0300

    Criar 05_consultas.sql
    Adição de consultas de teste

---
+++ banco/05_consultas.sql
@@ -0,0 +1,13 @@
+-- Emprestô - Consultas de teste
+
+-- Consultar pessoas
+SELECT * FROM pessoa;
+
+-- Consultar livros
+SELECT * FROM livro;
+
+-- Consultar empréstimos
+SELECT * FROM emprestimo;
+
+-- Consultar fila de espera
+SELECT * FROM fila_espera;
