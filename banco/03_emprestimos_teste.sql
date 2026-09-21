99d368f2534e6e58f9865139c3a29580e82e66df
ketelynlopes <ketelynlopespereira@gmail.com>
2026-09-21 08:58:27 -0300

    Excluir 03_emprestimos_teste.sql

--- banco/03_emprestimos_teste.sql
+++
@@ -1,7 +0,0 @@
--- Emprestô - Empréstimos de teste
-
-INSERT INTO emprestimo
-(id_pessoa, id_livro, data_emprestimo, prazo_devolucao, devolucao, atraso)
-VALUES
-(1, 1, '2026-09-20', '2026-09-27', 'Não feita', 'Não'),
-(2, 2, '2026-09-18', '2026-09-25', 'Feita', 'Não');
